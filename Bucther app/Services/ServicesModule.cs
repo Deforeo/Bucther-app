dceb95c7b9845447e4d1fd01776ea6050f6bec82
Student <Student@HOMENET157>
2026-06-27 11:02:41 +0300

    Servisec

---
+++ Bucther app/Services/ServicesModule.cs
@@ -0,0 +1,28 @@
+﻿using Bucther_app.Services.Implementations;
+using Bucther_app.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucther_app.Services
+{
+    public static class ServicesModule
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ISessionService, SessionService>();
+            services.AddScoped<INavigationService, NavigationService>(); // будет заменено позже
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IReservationService, ReservationService>();
+            services.AddScoped<IShiftService, ShiftService>();
+            services.AddScoped<IKitchenService, KitchenService>();
+            services.AddScoped<IAdminService, AdminService>();
+            services.AddScoped<ITableService, TableService>();
+
+            return services;
+        }
+    }
+}
